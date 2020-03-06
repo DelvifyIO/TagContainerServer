2ab5b758164ca6988e81367ba5256911bbc60b55
hooiyung <yung@delvify.io>
2020-03-06 12:56:47 +0800

    add production db

--- src/config/database.js
+++ src/config/database.js
@@ -3,6 +3,10 @@ module.exports = {
         development: {
             server: 'localhost:27017',
             database: 'delvify_tag',
+        },
+        production: {
+            server: 'localhost:27017',
+            database: 'delvify_tag',
         }
     }
 };
